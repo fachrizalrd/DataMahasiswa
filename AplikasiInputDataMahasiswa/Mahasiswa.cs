0d23d239deb1172c8d89dd6c0055446910cbd83c
Fachrizal Rafif Deninsyah <48305283+fachrizalrd@users.noreply.github.com>
2019-10-11 10:29:10 +0700

    Add files via upload

---
+++ AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    class Mahasiswa
+    {
+        public string Nama { set; get; }
+        public string NIM { set; get; }
+        public int Nilai { set; get; }
+        public string Kelas { set; get; }
+        public string Huruf { set; get; }
+    }
+}
